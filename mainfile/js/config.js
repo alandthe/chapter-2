478488b09a0a2bc0b652f38b1538043ffafafc08
alandthe <topeditz.business@gmail.com>
2025-04-09 20:05:51 +0300

    Create config.js

---
+++ mainfile/js/config.js
@@ -0,0 +1,17 @@
+// Chain Config
+export const SONIC_CHAIN = {
+  id: 146,
+  name: "Sonic",
+  rpcUrl: "https://rpc.sonic.network",
+  explorer: "https://sonicscan.io"
+};
+
+// Contracts
+export const CONTRACTS = {
+  dex: "0x00c8e7be475002587cf6880b096bedbb60d19ef6",
+  wcToken: "0xDD175A3998D81C3Ef51aBB9c1Eab2D7a8C795F68", 
+  sonicToken: "0x039e2fB66102314Ce7b64Ce5Ce3E5183bc94aD38"
+};
+
+// WalletConnect
+export const WALLETCONNECT_PROJECT_ID = "1d7b16153de36a7ee42b7f5c64458846";
